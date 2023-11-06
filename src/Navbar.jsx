ecd7428ebbcb3fee213b3cc2e96a7d9e3775da39
A. M. Rafinul Huq <amrh.cse@gmail.com>
2023-11-07 03:25:08 +0600

    "bug fixed"

---
+++ src/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Navbar = () =>{
+    return (
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Services</li>
+                <li>Contact Us</li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
